325beb18bce92a8375b82f3c5be949eb28f006f8
Kerry Johnson <kerry.t.johnson@gmail.com>
2019-04-06 15:02:06 -0600

    Add production configuration

---
+++ config/env/production.js
@@ -0,0 +1,10 @@
+module.exports = {
+    env: 'production',
+    db: 'mongodb://heroku_lzbg9002:f2b7fmincasoldaqhujmmciknm@ds263707.mlab.com:63707/heroku_lzbg9002',
+    port: 3000,
+    album_root: './albums-production',
+    archive_root: './albums-archive-production',
+    file_log_level: 'info',
+    console_log_level: 'error',
+    session_secret: 'msse663'
+}
